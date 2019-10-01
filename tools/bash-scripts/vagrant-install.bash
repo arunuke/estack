a0e1ba8a9ed5979c2a89c69749050bc9618585ff
arunt <ubuntu@intent-dove.maas>
2019-10-01 17:20:37 +0000

    some more commits

--- tools/bash-scripts/vagrant-install.bash
+++ tools/bash-scripts/vagrant-install.bash
@@ -3,5 +3,6 @@
 
 wget https://releases.hashicorp.com/vagrant/2.2.5/vagrant_2.2.5_linux_amd64.zip
 unzip vagrant_2.2.5_linux_amd64.zip
+sudo mv vagrant /usr/local/bin
+rm vagrant_2.2.5_linux_amd64.zip
 
-sudo apt-get install vagrant
